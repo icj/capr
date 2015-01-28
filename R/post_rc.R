664bb0fa27f9cb3b061569a218ba3bcee6a70107
icj <icj@email.arizona.edu>
2015-01-28 11:09:23 -0700

    removed certificate from post_rc()

--- R/post_rc.R
+++ R/post_rc.R
@@ -4,8 +4,6 @@
 #' and the inputs for the REDCap API (it will fail without them).
 #' 
 #' @param uri The uri for your REDCap project.
-#' @param certificate Certificate file (with path) containing REDCap security 
-#'   certificate.
 #' @param verbose Logical (default = FALSE). Verbose output from the posting.
 #' @param ... REDCap API inputs to send in the post.
 #' @return If inputs supplied, then a Large Character object; else an error.
@@ -14,6 +12,5 @@ post_rc <- function(uri, certificate, verbose = FALSE, ...) {
                   ...,
                   .opts = RCurl::curlOptions(ssl.verifypeer = TRUE,
                                              forbid.reuse = TRUE,
-                                             cainfo = certificate,
                                              verbose = verbose))
 }
